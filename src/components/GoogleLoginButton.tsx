6ede2a4f7cd251e9a6e3c12cc4d26e2591e00784
datnq <datnq2003@gmail.com>
2025-04-27 18:59:04 +0700

    fix: login with google

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -117,7 +117,7 @@ const GoogleLoginButton = () => {
 
   return (
     <div className="mt-4">
-      {/* Custom styled button */}
+      {/* Custom styled button
       <button
         className="flex items-center justify-center w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition"
         onClick={() => {
@@ -125,12 +125,12 @@ const GoogleLoginButton = () => {
         }}
       >
         <FcGoogle className="mr-2" size={20} /> Login with Google
-      </button>
+      </button> */}
       <GoogleLogin
         onSuccess={handleSuccess}
         onError={handleError}
         useOneTap
-        // className="hidden"
+        // className="flex items-center justify-center w-full py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition"
       />
 
       {/* Modal để nhập mật khẩu mới */}
